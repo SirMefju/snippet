9ec3c7dcf1cb03efdd5f56d751c13135f1186ceb
Mateusz Cieslik <mat.cieslik98@gmail.com>
2021-10-27 20:17:23 +0200

    Add files via upload
    c raspberry pi

---
+++ c - raspberry pi/led.c
@@ -0,0 +1,12 @@
+#include <wiringPi.h>
+int main (void)
+{
+  wiringPiSetup () ;
+  pinMode (29, OUTPUT) ;
+  for (;;)
+  {
+    digitalWrite (29, HIGH) ; delay (500) ;
+    digitalWrite (29,  LOW) ; delay (500) ;
+  }
+  return 0 ;
+}
